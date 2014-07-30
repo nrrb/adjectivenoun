fdac0ba698d6671d9b17a2a858af4c2e5eb9ba0d
Nick Bennett <nbennett@vivelohoy.com>
2014-07-30 18:19:17 -0500

    Changed output to Adjective Adjective Noun.

--- print_random.py
+++ print_random.py
@@ -10,7 +10,8 @@ with open('terms_by_pos.json', 'rb') as f:
 suitable_nouns = filter(lambda word: re.match(word_pattern, word), data['Noun'])
 suitable_adjectives = filter(lambda word: re.match(word_pattern, word), data['Adjective'])
 
-for i in range(10):
-    adjective = random.choice(suitable_adjectives)
+for i in range(100):
+    adjective1 = random.choice(suitable_adjectives)
+    adjective2 = random.choice(suitable_adjectives)
     noun = random.choice(suitable_nouns)
-    print '{} {}'.format(adjective.capitalize(), noun.capitalize())
+    print '{} {} {}'.format(adjective1.capitalize(), adjective2.capitalize(), noun.capitalize())
